a39fd0f9b6a4de2951ac098f7dcf12466b53c210
Mohil Thummar <mohil.0510@gmail.com>
2024-10-02 23:30:25 +0530

    Image dialog add

--- lib/views/dashboard/dashboard_view.dart
+++ lib/views/dashboard/dashboard_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 import 'package:task_app/core/constants/app_assets.dart';
 import 'package:task_app/core/constants/app_colors.dart';
 import 'package:task_app/core/constants/app_strings.dart';
+import 'package:task_app/core/responsive.dart';
 import 'package:task_app/core/utils/number_format.dart';
 import 'package:task_app/data/models/image_model.dart';
 import 'package:task_app/theme/app_style.dart';
@@ -23,12 +25,12 @@ class DashboardView extends GetView<DashboardController> {
         title: const Text(AppStrings.appName),
         backgroundColor: AppColors.primaryColor,
       ),
-      body: dashboardView(),
+      body: dashboardView(context),
     );
   }
 
   /// Dashboard Image Grid View
-  Widget dashboardView() {
+  Widget dashboardView(BuildContext context) {
     return Obx(() {
       if (controller.isFetching.value) {
         return const AppLoader();
@@ -46,7 +48,7 @@ class DashboardView extends GetView<DashboardController> {
               crossAxisSpacing: defaultPadding,
               mainAxisSpacing: defaultPadding,
             ),
-            itemBuilder: (context, index) => imageView(controller.imageList[index]),
+            itemBuilder: (context, index) => imageView(controller.imageList[index], context),
           );
         }
       }
@@ -54,34 +56,90 @@ class DashboardView extends GetView<DashboardController> {
   }
 
   /// Image View
-  Widget imageView(ImageModel image) {
-    return Container(
-      clipBehavior: Clip.antiAlias,
-      decoration: BoxDecoration(
-        color: AppColors.hintColor,
-        borderRadius: BorderRadius.circular(defaultRadius),
+  Widget imageView(ImageModel image, BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) => imageDialogView(image, context),
+        );
+      },
+      child: Container(
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          color: AppColors.hintColor,
+          borderRadius: BorderRadius.circular(defaultRadius),
+        ),
+        child: Stack(
+          alignment: Alignment.bottomRight,
+          children: [
+            AppImageView(
+              imagePath: image.webformatUrl ?? "",
+              fit: BoxFit.cover,
+              height: Get.width,
+              width: Get.height,
+            ),
+            Padding(
+              padding: EdgeInsets.all(defaultPadding / 2),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  countView(image: AppAssets.icView, value: (image.views?.numberFormat() ?? "").toString()),
+                  SizedBox(width: defaultPadding / 2),
+                  countView(image: AppAssets.icLike, value: (image.likes?.numberFormat() ?? "").toString()),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  /// Image dialog view
+  imageDialogView(ImageModel image, BuildContext context) {
+    return Dialog(
+      alignment: Alignment.center,
+      backgroundColor: Colors.transparent,
+      insetPadding: EdgeInsets.all(defaultPadding),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(defaultRadius)),
       child: Stack(
-        alignment: Alignment.bottomRight,
+        alignment: Alignment.topRight,
         children: [
           AppImageView(
             imagePath: image.webformatUrl ?? "",
+            borderRadius: BorderRadius.circular(defaultRadius - 4),
             fit: BoxFit.cover,
-            height: Get.width,
-            width: Get.height,
-          ),
-          Padding(
+            height: image.webformatHeight?.toDouble() ?? 400,
+            width: image.webformatWidth?.toDouble() ?? 400,
             padding: EdgeInsets.all(defaultPadding / 2),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                countView(image: AppAssets.icView, value: (image.views?.numberFormat() ?? "").toString()),
-                SizedBox(width: defaultPadding / 2),
-                countView(image: AppAssets.icLike, value: (image.likes?.numberFormat() ?? "").toString()),
-              ],
-            ),
+            margin: Responsive.isMobile(context) ? const EdgeInsets.only(top: 38) : const EdgeInsets.only(top: 32, right: 32),
+            backgroundColor: AppColors.white,
           ),
+          closedButton(context),
         ],
+      ),
+    );
+  }
+
+  /// Dialog close button
+  closedButton(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(color: AppColors.white, width: 2),
+      ),
+      child: IconButton(
+        onPressed: () => context.pop(),
+        padding: EdgeInsets.zero,
+        alignment: Alignment.center,
+        visualDensity: VisualDensity.standard,
+        constraints: BoxConstraints.tight(Size(28, 28)),
+        icon: const Icon(
+          Icons.close_rounded,
+          color: AppColors.white,
+          size: 20,
+        ),
       ),
     );
   }
